e419b8acb452a0dab197fafb89bf57fbe91c9e62
Katrina L Hill <98134026+katrina-l-hill@users.noreply.github.com>
2022-02-13 15:53:57 -0800

    I solved probThree

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
+  let sum = a + b
   return [a+b, `The sum of ${a} and ${b} is ${a+b}.`];
 }
 
@@ -48,24 +49,24 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
-// Katrina's notes:
-// function keyword function name(parameters) {
-//  return [parameter arithmetic operator parameter, `concated string.`]
-// 
-// }
+
+
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-  let firstSum = sum(a, b);
-  let secondSum = sum(firstSum, c);
-  let firstProduct = product(a, b);
-  let secondProduct = (firstProduct, c);
-  return [secondSum, secondProduct], `The sum of ${a} and ${b} and ${c} is ${a+b+c}. The product of ${a} and ${b} and ${c} is ${a*b*c}.';
+  // let sumFirst = sum(a, b)[0];
+  // let sumSecond = sum(sumFirst, c)[0];
+  // let multiplyFirst = multiply(a, b)[0];
+  // let multiplySecond = multiply(multiplyFirst, c)[0];
+  // let sprOne = `${a} and ${b} and ${c} sum to ${sumSecond}.`;
+  // let sprTwo = `The product of ${a} and ${b} and ${c} is ${multiplySecond}.`;
+  // return [sumSecond, multiplySecond, sprOne, sprTwo];
+  
 }
 
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-testSumAndMultiply(4,7,5);
+// testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -83,12 +84,15 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+  let sumFirst = sum(sumArr[0], sumArr[1])[0];
+  let sumSecond = sum(sumFirst, sumArr[2])[0];
+  let sprOnly = `${sumArr} was passed in as an array of numbers, and ${sumSecond} is their sum.`;
+  return [sumSecond, sprOnly];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -103,7 +107,7 @@ IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
-function multiplyArray(multArr) { //eslint-disable-line
+function multiplyArray() { //eslint-disable-line
 
 }
 
@@ -131,7 +135,7 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 let testDynamicArray = [1,2,3,4,5]; //eslint-disable-line
 
-function multiplyAnyArray(dynamicArray) { //eslint-disable-line
+function multiplyAnyArray() { //eslint-disable-line
 
 }
 
