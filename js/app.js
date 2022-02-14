9f3f4c35b727991b52ceb0019b83352986e9983e
Katrina L Hill <98134026+katrina-l-hill@users.noreply.github.com>
2022-02-13 16:03:02 -0800

    I solved probFour

--- js/app.js
+++ js/app.js
@@ -54,19 +54,19 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-  // let sumFirst = sum(a, b)[0];
-  // let sumSecond = sum(sumFirst, c)[0];
-  // let multiplyFirst = multiply(a, b)[0];
-  // let multiplySecond = multiply(multiplyFirst, c)[0];
-  // let sprOne = `${a} and ${b} and ${c} sum to ${sumSecond}.`;
-  // let sprTwo = `The product of ${a} and ${b} and ${c} is ${multiplySecond}.`;
-  // return [sumSecond, multiplySecond, sprOne, sprTwo];
+  let sumFirst = sum(a, b)[0];
+  let sumSecond = sum(sumFirst, c)[0];
+  let multiplyFirst = multiply(a, b)[0];
+  let multiplySecond = multiply(multiplyFirst, c)[0];
+  let sprOne = `${a} and ${b} and ${c} sum to ${sumSecond}.`;
+  let sprTwo = `The product of ${a} and ${b} and ${c} is ${multiplySecond}.`;
+  return [sumSecond, multiplySecond, sprOne, sprTwo];
   
 }
 
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
